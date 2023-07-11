17e5911ad2e0d0a11efd5a615f59f008c8e0dd2d
joshlynj <joshlyn294@gmail.com>
2023-07-11 15:59:38 -0600

    made some changes to the .py file

--- src/final.py
+++ src/final.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-ds_df = pd.read_csv('../data/ds_salaries.csv')
+ds_df = pd.read_csv('/Users/joshy/Desktop/dai/daimil5/projects/midterm/ds_salaries/data/ds_salaries.csv')
 
 
 def plot_highest_pay():
@@ -27,5 +27,33 @@ def plot_highest_pay():
     return fig, ax1
 
 
+def create_us_only():
+    ft_only_df = ds_df.drop(ds_df[ds_df['employment_type'] != "FT"].index)
+    us_ft_df = ft_only_df.drop(ft_only_df[ft_only_df['company_location']
+                                          != "US"].index)
+    return us_ft_df
+
+def plot_most_common(us_ft_df):
+    most_common_df = us_ft_df.groupby("job_count", 
+                                      as_index=False)[["salary_in_usd",
+                                                       "job_title"]].mean(numeric_only=True).sort_values(by = 'job_count', ascending=False).head(10)
+
+    index_labels = ["Data Engineer", 
+            "Data Scientist", 
+            "Data Analyst", 
+            "Machine Learning Engineer", 
+            "Data Architect",
+            "Analytics Engineer", 
+            "Applied Scientist", 
+            "Research Scientist", 
+            "Data Science Manager", 
+            "Research Engineer"]
+
+    most_common_df.index = index_labels
+    return most_common_df
+
+
+
 if __name__ == '__main__':
     plot_highest_pay()
+    create_us_only()
