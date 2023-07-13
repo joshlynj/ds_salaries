2df3bd8f2f1d9d8ee5b9c33bfd47cf4da9354dd0
joshlynj <joshlyn294@gmail.com>
2023-07-13 10:09:34 -0600

    refactored the code to ignore the original data masks. also considered employee residency as well

--- src/clean.py
+++ src/clean.py
@@ -4,27 +4,38 @@ ds_df = pd.read_csv('/Users/joshy/Desktop/dai/daimil5/projects/'
                     'midterm/ds_salaries/data/ds_salaries.csv')
 
 
-def create_highest_pay(ds_df):
+def create_us_only(ds_df):
+    ''' Function takes in a dataframe
+    Returns a dataframe that only shows U.S. entries'''
+    ft_only_df = ds_df.drop(ds_df[ds_df['employment_type'] != "FT"].index)
+    us_ft_res_df = ft_only_df \
+        .drop(ft_only_df[ft_only_df['company_location'] != "US"].index)
+    us_ft_res_df = us_ft_res_df \
+        .drop(us_ft_res_df[us_ft_res_df['employee_residence'] != "US"].index)
+    return us_ft_res_df
+
+
+def create_highest_pay(us_ft_res_df):
     ''' Function takes in a dataframe
         Returns a dataframe that is grouped by the Job Title and Includes:
         Salary in USD, Employment Type, and Company Location'''
-    highest_pay = ds_df.groupby("job_title")["salary_in_usd",
-                                             "employment_type",
-                                             "company_location"] \
-        .min().sort_values(by='salary_in_usd', ascending=False)
+    highest_pay = us_ft_res_df.groupby("job_title")["salary_in_usd",
+                                                    "employment_type",
+                                                    "company_location"] \
+        .max().sort_values(by='salary_in_usd', ascending=False)
     mask = highest_pay['company_location']\
         .isin(["US"]) & highest_pay['employment_type'].isin(["FT"])
     highest_pay_top = highest_pay[mask].head(10)
     return highest_pay_top
 
 
-def create_lowest_pay(ds_df):
+def create_lowest_pay(us_ft_res_df):
     ''' Function takes in a dataframe
     Returns a dataframe that is grouped by the Job Title and Includes:
     Salary in USD, Employment Type, and Company Location'''
-    lowest_pay = ds_df.groupby("job_title")["salary_in_usd",
-                                            "employment_type",
-                                            "company_location"]\
+    lowest_pay = us_ft_res_df.groupby("job_title")["salary_in_usd",
+                                                   "employment_type",
+                                                   "company_location"]\
         .min().sort_values(by='salary_in_usd',
                            ascending=True)
     mask = lowest_pay['company_location']\
@@ -33,28 +44,19 @@ def create_lowest_pay(ds_df):
     return lowest_pay_top
 
 
-def create_us_only(ds_df):
-    ''' Function takes in a dataframe
-    Returns a dataframe that only shows U.S. entries'''
-    ft_only_df = ds_df.drop(ds_df[ds_df['employment_type'] != "FT"].index)
-    us_ft_df = ft_only_df \
-        .drop(ft_only_df[ft_only_df['company_location'] != "US"].index)
-    return us_ft_df
-
-
-def add_job_count(us_ft_df):
+def add_job_count(us_ft_res_df):
     '''Function takes in a dataframe
     Returns a dataframe with a count of the jobs'''
-    us_ft_df['job_count'] = us_ft_df\
+    us_ft_res_df['job_count'] = us_ft_res_df\
         .groupby('job_title')['job_title']\
         .transform('count')
-    return us_ft_df
+    return us_ft_res_df
 
 
-def create_most_common(us_ft_df):
+def create_most_common(us_ft_res_df):
     ''' Function takes in a dataframe 
         Returns a dataframe that shows '''
-    most_common_df = us_ft_df\
+    most_common_df = us_ft_res_df\
         .groupby("job_count",
                  as_index=False)[["salary_in_usd",
                                   "job_title"]]\
@@ -77,11 +79,11 @@ def create_most_common(us_ft_df):
     return most_common_df
 
 
-def create_remote_df(us_ft_df):
+def create_remote_df(us_ft_res_df):
     '''Function takes in a dataframe
         Returns a data frame with only the remote ratio and company size'''
-    remote_df = us_ft_df[['remote_ratio',
-                          "company_size"]]\
+    remote_df = us_ft_res_df[['remote_ratio',
+                              "company_size"]]\
         .sort_values(by='company_size',
                      ascending=False)
     return remote_df
@@ -108,12 +110,12 @@ def create_l_df(remote_df):
     return l_df
 
 
-def create_exp_df(us_ft_df):
+def create_exp_df(us_ft_res_df):
     '''Function that takes in the U.S. only dataframe
     Returns a new dataframe with select columns'''
-    exp_df = us_ft_df[['experience_level',
-                       "salary_in_usd",
-                       "job_title"]]\
+    exp_df = us_ft_res_df[['experience_level',
+                           "salary_in_usd",
+                           "job_title"]]\
         .sort_values(by='salary_in_usd',
                         ascending=False)
     return exp_df
